19112f9759407237a2e73d27db33b6b05bbde983
Silvestru <silvestru.state17@gmail.com>
2023-07-25 17:30:46 +0300

    adaugat navbar

---
+++ app_retete/src/Components/Navbar.js
@@ -0,0 +1,24 @@
+
+function Navbar() {
+    return(
+        <nav className="navBar">
+            <ul>
+                <li>
+                    <h1>Retete</h1>
+                </li>
+                <li>
+                    <h1><a href="/">Favorite</a></h1>
+                </li>
+                <li>
+                    <h1><a href="/">Adauga o reteta</a></h1>
+                </li>
+                <li>
+                    <h1><a href="/">Detalii reteta</a></h1>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+
+export default Navbar;
